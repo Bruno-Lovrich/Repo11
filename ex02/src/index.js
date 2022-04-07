660d8743ba4b5a86b5a30eab99927d9865fde879
Bruno Lovric <brunoolovric@gmail.com>
2022-04-07 22:36:21 +0200

    Array methods

---
+++ ex02/src/index.js
@@ -0,0 +1,13 @@
+var myStr = "Paragon is the best!";
+
+function useMethods(str) {
+
+  var myReverse = str.split("").reverse().join("");
+  var countCharacters = myReverse.split(" ").map(x=>x.length);
+
+  return { myReverse, countCharacters };
+}
+
+console.log(useMethods(myStr));
+
+module.exports = useMethods;
