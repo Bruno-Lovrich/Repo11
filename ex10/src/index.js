37b88a990fdc88e86495a97684dd69e5970ebdc1
Bruno Lovric <brunoolovric@gmail.com>
2022-04-08 07:51:52 +0200

    Falsy ejector

---
+++ ex10/src/index.js
@@ -0,0 +1,23 @@
+function myBouncer(arr) {
+  for (var i = 0; i < arr.length; i++) {
+    if (
+      arr[i] === false ||
+      arr[i] === null ||
+      arr[i] === 0 ||
+      arr[i] === "" ||
+      arr[i] === undefined ||
+      arr[i] !== arr[i]
+    ) {
+      arr.splice(i, 1);
+      i--;
+    }
+  }
+  return arr;
+}
+
+console.log(myBouncer([7, "ate", "", false, 9]));
+console.log(myBouncer(["a", "b", "c"]));
+console.log(myBouncer([false, null, 0, NaN, undefined, ""]));
+console.log(myBouncer([null, NaN, 1, 2, undefined]));
+
+module.exports = myBouncer;
